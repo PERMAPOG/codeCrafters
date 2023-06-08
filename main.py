b911a1e87d18fe36a11f97111f8fc2fa9d7c8d68
PERMAPOG <85780059+PERMAPOG@users.noreply.github.com>
2023-06-07 18:19:37 -0700

    revert to function

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-print("Hello, Creative Code Crafters!")
 
-
-
-
-
-print("swaggin baggin and dabbin")
-=======
 print("Hello, Kreative Kode Krafters!")
->>>>>>> 2d212b6fbd6d1f6dc102ddd627d527c9838aec77
+
+
+print("cool filters otw")
