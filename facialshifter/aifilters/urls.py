eae945b180c7d3051de3c31368f5dfef13f4b667
Josue Sanchez <30679533+jsanchez2388@users.noreply.github.com>
2023-07-03 18:58:45 -0700

    changes will be removed

--- facialshifter/aifilters/urls.py
+++ facialshifter/aifilters/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('cowboy/', views.cowboy_filter_view, name='cowboy'), 
+    path('apply_filter/', views.apply_filter_view, name='apply_filter'),
+    path('face_mesh/', views.face_mesh_view, name='face_mesh'),
+    path('face_mesh_page/', views.face_mesh_page_view, name='face_mesh_page'),
+    path('upload/', views.face_mesh_view, name='upload'), 
+    path('result/<str:img_url>/', views.result_view, name='result'),
 ]
