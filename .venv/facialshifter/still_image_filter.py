fa13443988a9d7314d0bfaa9394c11c9dfbd9049
PERMAPOG <85780059+PERMAPOG@users.noreply.github.com>
2023-07-11 00:45:56 -0700

    added pydoc to still_image_filter.py

--- .venv/facialshifter/still_image_filter.py
+++ .venv/facialshifter/still_image_filter.py
@@ -1,87 +1,139 @@
+"""
+still_image_filter.py
+
+Description: This code applies a filter to images, placing the filter onto faces detected in the images.
+
+Author: Christian Jaime
+Version: 1.0
+Date: 7/7/2023
+
+Modules:
+- cv2: OpenCV for image processing.
+- numpy: NumPy for numerical operations.
+- mediapipe: MediaPipe for face mesh detection.
+
+Functions:
+- read_image(image_file)
+- apply_filter(img, face_mesh_images, filter)
+- get_filter_params(face_landmarks, img_shape, filter_shape)
+- overlay_filter(img, new_filter, left_eye_corner, right_eye_corner)
+- display_images(original_images, filtered_images)
+- bossModeImage() [main function]
+"""
+
 import cv2
 import numpy as np
 import mediapipe as mp
 
-def bossModeImage():
-    # mediapipe face mesh 
-    face_mesh = mp.solutions.face_mesh
 
-    # read in filter & convert it from BGR to RGBA
-    bossMode = cv2.imread('../Static/images/bossMode.png', cv2.IMREAD_UNCHANGED)
-    bossMode = cv2.cvtColor(bossMode, cv2.COLOR_BGRA2RGBA)
+def read_image(image_file):
+    """
+    Reads an image from a file and converts it from BGR to RGB.
 
-    # parameters for face mesher
-    face_mesh_images = face_mesh.FaceMesh(static_image_mode=True, max_num_faces=3, min_detection_confidence=0.7)
+    Args:
+        image_file: The path of the image file.
+    
+    Returns:
+        The image in RGB.
+    """
+    img = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
 
-    # list of image paths
-    image_files = ['../Static/images/josue.jpg', '../Static/images/sample5.jpg']
+def apply_filter(img, face_mesh_images, filter):
+    """
+    Applies a filter to a face in an image.
 
-    # Lists to store original and filtered images
-    original_images = []
-    filtered_images = []
+    Args:
+        img: The image that the filter will be placed ontop of.
+        face_mesh_images: The Mediapipe FaceMesh object.
+        filter: The filter that will be applied.
+    
+    Returns:
+        The image with the filter applied.
+    """
 
-    for image_file in image_files:
+    face_mesh_results = face_mesh_images.process(img)
 
-        # read in image & convert BGR to RGB
-        img = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # if face mesh found in image
+    if face_mesh_results.multi_face_landmarks:
+        for face_landmarks in face_mesh_results.multi_face_landmarks:
 
-        # Store a copy of the original image
-        original_images.append(img.copy())
+            # calculates the desired width of the filter which is 3.3 times the distance between the eyes
+            left_eye_corner, right_eye_corner, ratio = get_filter_params(face_landmarks, img.shape, filter.shape)
 
-        # face mesh desired image
-        face_mesh_results = face_mesh_images.process(img)
+            # Resize the filter to fit the face width
+            if ratio > 0:
+                resized_filter = cv2.resize(filter, (0,0), fx=ratio, fy=ratio)
+                img = overlay_filter(img, resized_filter, left_eye_corner, right_eye_corner)
 
-        # if face mesh found in image
-        if face_mesh_results.multi_face_landmarks:
-            for face_landmarks in face_mesh_results.multi_face_landmarks:
+    return img
 
-                left_eye_corner = (int(face_landmarks.landmark[226].x * img.shape[1]), int(face_landmarks.landmark[226].y * img.shape[0]))
-                right_eye_corner = (int(face_landmarks.landmark[446].x * img.shape[1]), int(face_landmarks.landmark[446].y * img.shape[0]))
+def get_filter_params(face_landmarks, img_shape, filter_shape):
+    """
+    Calculates the parameters for applying filter.
 
-                # calculates the desired width of the filter which is three times the distance between the eyes
-                bossMode_width = abs(right_eye_corner[0] - left_eye_corner[0]) # Calculate width by difference in X-coordinates (0, 1 is y cord)
-                bossMode_width = int(3.3 * bossMode_width)
-                ratio = bossMode_width / bossMode.shape[1] 
+    Args:
+        face_landmarks: The detected face landmarks.
+        img_shape: The shape of the image.
+        filter_shape: The shape of the filter.
+    
+    Returns:
+        A args of the left eye corner, right eye corner, filter width and ratio.
+    """
+    left_eye_corner = (int(face_landmarks.landmark[226].x * img_shape[1]), int(face_landmarks.landmark[226].y * img_shape[0]))
+    right_eye_corner = (int(face_landmarks.landmark[446].x * img_shape[1]), int(face_landmarks.landmark[446].y * img_shape[0]))
 
-                # Resize the filter to fit the face width
-                if ratio > 0:
-                    new_bossMode = cv2.resize(bossMode, (0,0), fx=ratio, fy=ratio)
+    filter_width = abs(right_eye_corner[0] - left_eye_corner[0]) # Calculate width by difference in X-coordinates
+    filter_width = int(3.3 * filter_width)
+    ratio = filter_width / filter_shape[1]
 
-                    adjust_x = 10  # adjust these values based on your filter image
-                    adjust_y = 20
+    return left_eye_corner, right_eye_corner, ratio
 
-                    mid_point = ((right_eye_corner[0] + left_eye_corner[0]) // 2, (right_eye_corner[1] + left_eye_corner[1]) // 2)
-                    start_point = (mid_point[0] - new_bossMode.shape[1] // 2 - adjust_x, mid_point[1] - new_bossMode.shape[0] // 2 - adjust_y)
-                    start_point = (max(0, start_point[0]), max(0, start_point[1]))
+def overlay_filter(img, new_filter, left_eye_corner, right_eye_corner):
+    """
+    Overlays the filter onto the image.
 
-                    # Calculate the region size
-                    region_size = (min(new_bossMode.shape[0], img.shape[0] - start_point[1]), 
-                                min(new_bossMode.shape[1], img.shape[1] - start_point[0]))
+    Args:
+        img: The image that the filter will be applied to.
+        new_filter: The resized filter.
+        left_eye_corner: The coordinates of the left eye corner.
+        right_eye_corner: The coordinates of the right eye corner.
+    
+    Returns:
+        The image with the filter overlayed.
+    """
 
-                    # Select the region of the image where the filter will be applied
-                    img_region = img[start_point[1]:start_point[1] + region_size[0], start_point[0]:start_point[0] + region_size[1]]
-
-                    # Select the corresponding region from new_bossMode
-                    new_bossMode_region = new_bossMode[:region_size[0], :region_size[1]]
-
-                    alpha_bossMode = new_bossMode_region[:,:,3] / 255.0
-                    alpha_img = 1.0 - alpha_bossMode
-
-                    # Apply the filter to the region
-                    img_region[:,:,:3] = (alpha_bossMode[:,:,np.newaxis] * new_bossMode_region[:,:,:3] + 
-                                        alpha_img[:,:,np.newaxis] * img_region[:,:,:3])
+    
+    mid_point = ((right_eye_corner[0] + left_eye_corner[0]) // 2, (right_eye_corner[1] + left_eye_corner[1]) // 2) # // in Python performs integer floor division nessary for pixel placement
+    start_point = (mid_point[0] - new_filter.shape[1] // 2, mid_point[1] - new_filter.shape[0] // 2)
+    
+    # ensures that the starting point of the filter is within the image boundaries
+    start_point = (max(0, start_point[0]), max(0, start_point[1]))
+    img_region = img[start_point[1]:start_point[1] + new_filter.shape[0], start_point[0]:start_point[0] + new_filter.shape[1]]
 
 
-        # Store the filtered image
-        filtered_images.append(img)
+    #  the transparency level of a pixel (0-255)
+    alpha_filter = new_filter[:,:,3] / 255.0
+    alpha_img = 1.0 - alpha_filter
 
-    # display the original and filtered images
+    # applies the filter to image region
+    img_region[:,:,:3] = (alpha_filter[:,:,np.newaxis] * new_filter[:,:,:3] + alpha_img[:,:,np.newaxis] * img_region[:,:,:3])
+    
+    return img
+
+def display_images(original_images, filtered_images):
+    """
+    Displays the original and filtered images.
+
+    Args:
+        original_images: A list of original images.
+        filtered_images: A list of filtered images.
+    """
     for i in range(len(original_images)):
         original = original_images[i]
         filtered = filtered_images[i]
 
-        #display image with keyboard wait
         cv2.imshow('Original', cv2.cvtColor(original, cv2.COLOR_RGB2BGR))
         cv2.waitKey(0) 
 
@@ -92,3 +144,27 @@ def bossModeImage():
             break
 
     cv2.destroyAllWindows()
+
+def bossModeImage():
+    """
+    The main function that runs the script.
+    """
+    face_mesh = mp.solutions.face_mesh
+    imported_filter = cv2.imread('.venv/Static/images/bossMode.png', cv2.IMREAD_UNCHANGED)
+    imported_filter = cv2.cvtColor(imported_filter, cv2.COLOR_BGRA2RGBA)
+
+    face_mesh_images = face_mesh.FaceMesh(static_image_mode=True, max_num_faces=3, min_detection_confidence=0.7)
+
+    image_files = ['.venv/Static/images/sample7.jpg', '.venv/Static/images/sample2.jpg', '.venv/Static/images/sample8.jpg']
+
+    original_images = []
+    filtered_images = []
+
+    for image_file in image_files:
+        img = read_image(image_file)
+        original_images.append(img.copy())
+        img = apply_filter(img, face_mesh_images, imported_filter)
+        filtered_images.append(img)
+
+    display_images(original_images, filtered_images)
+
